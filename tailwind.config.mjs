dc66bec92084440339db535202fd33b27ea01c3e
mariocannon <cannons.for.u@gmail.com>
2024-10-13 19:54:02 +1300

    d

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,7 +8,14 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				primary: {"50":"#fafafa","100":"#f5f5f5","200":"#e5e5e5","300":"#d4d4d4","400":"#a3a3a3","500":"#737373","600":"#525252","700":"#404040","800":"#262626","900":"#171717","950":"#0a0a0a"}
+				primary: {
+					transparent: 'transparent',
+					current: 'currentColor',
+					"swrokslite":"#93bfcf",
+					"swroksdark":"#6096b4",
+					"950":"#0a0a0a"
+				
+				}
 			  }
 
 		},
